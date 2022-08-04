5375338dd802592e11c91383053f7e9238160f9a
Tautvydas-coder <b7&*%13kHqvP>
2022-08-04 11:54:56 +0300

    added and change to Firefox service and web

--- webdriver_service.py
+++ webdriver_service.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service as FoxService
 
 CHROME_DRIVER_PATH = "C:/Users/takvietk/Downloads/Programos/chromedriver_win32_103/chromedriver"
+FFOX_DRIVER_PATH = "C:/Users/takvietk/Downloads/Programos/geckodriver-v0.31.0-win64/geckodriver"
 
 
 def driver_service():
-    ser = Service(CHROME_DRIVER_PATH)
-    driver = webdriver.Chrome(service=ser)
+    # ser = Service(CHROME_DRIVER_PATH)
+    ser = FoxService(FFOX_DRIVER_PATH)
+    # driver = webdriver.Chrome(service=ser)
+    driver = webdriver.Firefox(service=ser)
     return driver
