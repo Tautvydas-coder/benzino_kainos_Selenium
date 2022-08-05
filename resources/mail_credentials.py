1ff88446703d128ecb9e3d0c41b5d9cdf7f664d6
Tautvydas-coder <b7&*%13kHqvP>
2022-08-05 14:04:32 +0300

    files are organised into folders and Paths,var,url are moved to variables.py

---
+++ resources/mail_credentials.py
@@ -0,0 +1,9 @@
+from resources.variables import GMAIL_NAME, GMAIL_PASSWORD
+
+
+def email_name():
+    return GMAIL_NAME
+
+
+def email_password():
+    return GMAIL_PASSWORD
---
+++ webdriver_service/activate_drivers.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service as FoxService
+
+from resources.variables import CHROME_DRIVER_PATH, FFOX_DRIVER_PATH
+
+
+def driver_service():
+    # ser = Service(CHROME_DRIVER_PATH)
+    ser = FoxService(FFOX_DRIVER_PATH)
+    # driver = webdriver.Chrome(service=ser)
+    driver = webdriver.Firefox(service=ser)
+    return driver
